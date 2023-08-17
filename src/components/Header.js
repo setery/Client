68d5bbb0aeb133b8c0897d4d94be7de626c483c0
setery <you@example.comsa.duery@gmail.com>
2023-08-17 17:04:07 -0400

    feat: Login&Register view

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+const Header = () => {
+  return (
+    <div className="main-header">
+      <div className="main-header__inner">
+        <div className="main-header__left">
+          <Link to="/">TodoList</Link>
+        </div>
+        <div className="main-header__right">
+            <button className="btn">Logout</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
