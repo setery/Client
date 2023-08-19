0acf7fa7c984a0a17fc3642134cfae5edf8e8444
setery <you@example.comsa.duery@gmail.com>
2023-08-18 20:40:58 -0400

    feat: ToDos dashboard & style

---
+++ src/components/Dashboard.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useGlobalContext } from "../context/GlobalContext";
+import { useNavigate } from "react-router-dom";
+import ToDoCard from "./ToDoCard";
+
+const Dashboard = () => {
+  const { user, completeToDos, incompleteToDos } = useGlobalContext();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (!user && navigate) {
+      navigate("/");
+    }
+  }, [user, navigate]);
+  return (
+    <div className="dashboard">
+      <div className="todos">
+        {incompleteToDos.map((toDo) => (
+          <ToDoCard toDo={toDo} key={toDo.uuid}/>
+        ))}
+      </div>
+      {completeToDos.length > 0 && <div className="todos">
+        <h2 className="todos__title">Complete ToDo's</h2>
+        {completeToDos.map(toDo=>(
+            <ToDoCard toDo={toDo} key={toDo.uuid}/>
+        ))}
+        </div>}
+    </div>
+  );
+};
+
+export default Dashboard;
